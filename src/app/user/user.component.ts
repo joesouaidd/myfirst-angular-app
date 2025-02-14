43536888da62f088efaffccd38e61d6e063bb288
Joe S <souaid.joe@hotmail.com>
2025-02-14 15:21:41 +0100

    input() function + Singal  instead of Input decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -14,21 +14,39 @@ export class UserComponent {
   //by default it is public so there is no need to add public
 
   //properties of the class
+
   //input decorator is used to make the property available to the parent component
+
   //this will mark the property as settable from outside
   //the ! is used to tell typescript that the property will be initialized later
-  @Input() avatar!: string; //this is the avatar of the user
-  @Input() name!: string; //this is the name of the user
+  // @Input({required: true}) avatar!: string; //this is the avatar of the user
+  // @Input({required: true}) name!: string; //this is the name of the user
 
 
-  get imagePath(){
+  //with signal + input()
 
-    return 'assets/users/' + this.avatar;
-  }
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+
+
+  //without signal
+  // get imagePath(){
+
+  //   return 'assets/users/' + this.avatar;
+  // }
+
+
+  //with signal
+  imagePath = computed(() => { 
+    return 'assets/users/' + this.avatar();
+  });
+
+
 
   //method that will be called by the event listener binded to the button
   onSelectUser(){
-
+    this.avatar.set('user-1.jpg');
   }
 
 }
