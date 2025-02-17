f9cee7b6d87975564aa21d9575031557b8af6a41
Joe S <souaid.joe@hotmail.com>
2025-02-17 15:17:22 +0100

    new-task component added and displayed, cancel button to return

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+
+  @Output () cancel = new EventEmitter<boolean>();
+
+
+  onCancelAddTask(){
+    this.cancel.emit(false);
+  }
+
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -10,6 +11,8 @@ export class TasksComponent {
 
   @Input({required:true}) taskowner ?: string;
   @Input({required:true}) userId ?: string;
+
+  isAddingTask :boolean = false;
 
   dummyTasks = [
     {
@@ -43,6 +46,14 @@ export class TasksComponent {
 
   onCompleteTask(id:string){
     this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+
+  }
+  onCanelAddTask(isCanceled : boolean){
+    this.isAddingTask = isCanceled;
   }
 
 }
